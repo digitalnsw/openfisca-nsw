06fbd6257d6f132b620736c772158349b5f7676c
TJ Harrop <tjharrop@gmail.com>
2020-08-23 04:02:32 +1000

    adding seniors enum - temp simplification for demo

--- openfisca_nsw/variables/policy/seniors/nsw_seniors_card.py
+++ openfisca_nsw/variables/policy/seniors/nsw_seniors_card.py
@@ -8,24 +8,44 @@
 from openfisca_core.model_api import *
 # Import the Entities specifically defined for this tax and benefit system
 from openfisca_nsw.entities import *
+from openfisca_nsw.variables.return_type import *
 
 
 class nsw_seniors_card_works_under_20hrs(Variable):
-    value_type = bool
+    value_type = Enum
+    possible_values = ReturnType
     entity = Person
     definition_period = MONTH
+    default_value = ReturnType.unknown
     label = "Whether the person works under 20 hours a week over a 12 month period"
 
 
 class nsw_seniors_card_person_is_eligible(Variable):
-    value_type = bool
+    value_type = Enum
+    possible_values = ReturnType
     entity = Person
     definition_period = MONTH
+    default_value = ReturnType.unknown
     label = "Whether the person is eligible for a NSW seniors card"
 
     def formula(persons, period, parameters):
-        return (
-            (persons('age', period) >= parameters(period).nsw_seniors_card.min_age)
-            * persons('is_permanent_nsw_resident', period)
-            * persons('nsw_seniors_card_works_under_20hrs', period)
+
+        calc_age = persons('age', period)
+        # calc_is_permanent_nsw_resident = persons('is_permanent_nsw_resident', period)
+        calc_nsw_seniors_card_works_under_20hrs = persons('nsw_seniors_card_works_under_20hrs', period)
+
+        is_disqualified = (
+            (calc_age < parameters(period).nsw_seniors_card.min_age)
+            # + not_(persons('is_permanent_nsw_resident', period))
+            + (calc_nsw_seniors_card_works_under_20hrs == calc_nsw_seniors_card_works_under_20hrs.possible_values.no)
             )
+
+        is_qualified = (
+            (calc_age >= parameters(period).nsw_seniors_card.min_age)
+            # * persons('is_permanent_nsw_resident', period)
+            * (calc_nsw_seniors_card_works_under_20hrs == calc_nsw_seniors_card_works_under_20hrs.possible_values.yes)
+            )
+
+        return select(
+            [is_disqualified, is_qualified],
+            [ReturnType.disqualified, ReturnType.qualified], default=ReturnType.unknown)
