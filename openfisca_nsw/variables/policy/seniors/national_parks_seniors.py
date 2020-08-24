c46b456ecdbdc13be3aa1bd7753b48d923348828
TJ Harrop <tjharrop@gmail.com>
2020-08-25 02:22:06 +1000

    converted national parks seniors

--- openfisca_nsw/variables/policy/seniors/national_parks_seniors.py
+++ openfisca_nsw/variables/policy/seniors/national_parks_seniors.py
@@ -8,15 +8,28 @@
 from openfisca_core.model_api import *
 # Import the Entities specifically defined for this tax and benefit system
 from openfisca_nsw.entities import *
+from openfisca_nsw.variables.return_type import *
 
 
 class national_parks_seniors_person_is_eligible(Variable):
-    value_type = bool
+    value_type = Enum
+    possible_values = ReturnType
     entity = Person
+    default_value = ReturnType.unknown
     definition_period = MONTH
     label = "Whether the person is eligible for seniors' discount annual park passes at approx 20% discount. https://www.nationalparks.nsw.gov.au/passes-and-fees/discount-and-concession-passes#seniors-discount-annual-park-passes"
 
     def formula(persons, period, parameters):
-        return (
-            persons('has_any_seniors_card', period)
+        calc_any_card = persons('has_any_seniors_card_enum', period)
+
+        is_disqualified = (
+            (calc_any_card == calc_any_card.possible_values.no)
             )
+
+        is_qualified = (
+            (calc_any_card == calc_any_card.possible_values.yes)
+            )
+
+        return select(
+            [is_disqualified, is_qualified],
+            [ReturnType.disqualified, ReturnType.qualified], default=ReturnType.unknown)
